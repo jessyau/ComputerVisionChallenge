230963e0f15fd2c61d86e6b9161368ba8c64fbd9
Jess <Jessica@Jessicas-MacBook-Pro.local>
2015-02-16 14:43:24 -0600

    Initial Commit

---
+++ ComputerVisionChallenge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ComputerVisionChallenge
+//
+//  Created by Jessica Yau on 2015-02-16.
+//  Copyright (c) 2015 Jessica Yau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
